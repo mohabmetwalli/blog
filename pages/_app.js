f47ff2e7f48e3a90bcfec7f8a20cb024f50a3cf7
mohab <mohabmetwalli@tutanota.com>
2022-11-03 21:21:12 +0200

    Use next/script to load highlight.js theme.

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
 import '../styles/globals.css'
 import { MDXProvider } from '@mdx-js/react'
 import MDXComponents from '../components/MDXComponents'
-import Script from 'next/script'
 import Layout from '../layouts/Layout'
 
 export default function MyApp ({ Component, pageProps }) {
   return (
     <MDXProvider components={MDXComponents}>
-      <Script src='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css' />
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function MyDocument () {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <Script src='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css' />
+      </Head>
       <body>
         <Main />
         <NextScript />
