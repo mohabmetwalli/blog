b2744454d0a89714efed53a14b5f5489a79ce7c8
mohab <mohabmetwalli@tutanota.com>
2022-11-03 21:17:49 +0200

    Use next/script to load highlight.js theme.

--- pages/_document.js
+++ pages/_document.js
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function MyDocument () {
   return (
     <Html lang='en'>
-      <Head>
-        <link
-          rel='stylesheet'
-          href='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css'
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
