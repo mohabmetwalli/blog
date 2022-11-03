bcc502bde490be5be16a6a7ab9024876ae2ec5cb
mohab <mohabmetwalli@tutanota.com>
2022-11-03 21:40:45 +0200

    Preload highlight.js theme.

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,7 @@ export default function MyDocument () {
   return (
     <Html lang='en'>
       <Head>
-        <link
-          rel='stylesheet'
-          href='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css'
-        />
+        <link rel='preload' href='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css' as='style' onload="this.onload=null;this.rel='stylesheet'" />
       </Head>
       <body>
         <Main />
